3e2b3f595565c66420f6af15248be26ce20e2df3
Ben Lerner <benlerner95@gmail.com>
2015-11-07 14:07:00 -0500

    quick stack-based parenthetical validator

---
+++ parenvalidator.py
@@ -0,0 +1,24 @@
+def validate(string):
+    stack = []
+    parens = {"(": ")",
+              "{": "}",
+              "[": "]"}
+    for i in string:
+        if i in parens.keys(): stack.append(i)
+        if i in parens.values():
+            if not stack: return False
+            leftParen = stack.pop()
+            if parens[leftParen] is not i:
+                return False
+
+    return True if not stack else False
+
+def main():
+    a = "{}()[])"
+    b = "{[(])}"
+    c = "{[()]}"
+    print(validate(a))
+    print(validate(b))
+    print(validate(c))
+
+main()
