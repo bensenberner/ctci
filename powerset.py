78384a3a086bd8109e5dcfdd1391f45fcb5e61ea
Ben Lerner <benlerner95@gmail.com>
2015-11-07 00:28:41 -0500

    reworded

--- powerset.py
+++ powerset.py
@@ -1,4 +1,6 @@
 def makeSets(arr):
+    # if I did this in a strongly typed language then I would have
+    # to make the size of sets n(n+1)/2
     sets = []
     for setSize in range(0, len(arr) + 1):
         for i in range(0, len(arr) - setSize):
@@ -6,5 +8,5 @@ def makeSets(arr):
     return sets
 
 if __name__ == "__main__":
-    a = [2, 3, 4, 1]
+    a = [2, 4, 1, 3]
     print(makeSets(a))
