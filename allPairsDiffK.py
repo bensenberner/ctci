2c78ac35cef5ce4bbefc1f045d44f65ac28c428c
Ben Lerner <benlerner95@gmail.com>
2016-01-24 09:08:33 -0500

    done

--- allPairsDiffK.py
+++ allPairsDiffK.py
@@ -1,17 +1,17 @@
 def findAllPairs(nums, k):
     recordedNums = set()
-    pairs = []
+    pairs = set()
     for num in nums:
-        if num - k in recordedNums:
-            pairs.append([num, num - k])
-        if num + k in recordedNums:
-            pairs.append([num, num + k])
+        if num - k in recordedNums and (num - k, num) not in pairs:
+            pairs.add((num, num - k))
+        if num + k in recordedNums and (num + k, num) not in pairs:
+            pairs.add((num, num + k))
         recordedNums.add(num)
     return pairs
 
 def main():
-    nums = [1, 3, 9, 6, 2, 8, 12, 10, 1]
-    k = 3
+    nums = [1, 3, 9, 6, 2, 8, 12, 10, 1, 3, 3]
+    k = 2
     print(findAllPairs(nums, k))
 
 main()
