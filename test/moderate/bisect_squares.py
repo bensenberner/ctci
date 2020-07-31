6c96e7d85802fb81ff381ddb59b96b330359b4e9
Ben Lerner <benlerner95@gmail.com>
2020-07-31 12:22:53 -0400

    handled unusual cases

--- test/moderate/bisect_squares.py
+++ test/moderate/bisect_squares.py
@@ -24,3 +24,17 @@ class Test(TestCase):
         line = find_bisection_line(sq1, sq2)
         expected = Line(slope=None, y_or_x_intercept=3)
         self.assertEqual(expected, line)
+
+    def test_same_square(self):
+        sq1 = Square(top_left=Point(0, 2), length=2)
+        sq2 = Square(top_left=Point(0, 2), length=2)
+        line = find_bisection_line(sq1, sq2)
+        expected = Line(slope=None, y_or_x_intercept=1)
+        self.assertEqual(expected, line)
+
+    def test_same_middle_different_square(self):
+        sq1 = Square(top_left=Point(0, 2), length=4)
+        sq2 = Square(top_left=Point(1, 1), length=2)
+        line = find_bisection_line(sq1, sq2)
+        expected = Line(slope=None, y_or_x_intercept=2)
+        self.assertEqual(expected, line)
