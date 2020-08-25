945bd055296f40a049be0a6f7126a7aa73bbaa0b
Ben Lerner <benlerner95@gmail.com>
2020-08-24 20:43:44 -0400

    breaking words

---
+++ test/hard/word_break_ii_140.py
@@ -0,0 +1,30 @@
+import unittest
+
+from hard.word_break_ii_140 import wordBreak
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = "catsanddog"
+        word_dict = ["cat", "cats", "and", "sand", "dog"]
+        expected = {"cats and dog", "cat sand dog"}
+        self.assertEqual(expected, wordBreak(s, word_dict))
+
+    def test_dumb(self):
+        self.assertEqual([], wordBreak("", ["a", "b", "c'"]))
+
+    def test_long(self):
+        s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        word_dict = [
+            "a",
+            "aa",
+            "aaa",
+            "aaaa",
+            "aaaaa",
+            "aaaaaa",
+            "aaaaaaa",
+            "aaaaaaaa",
+            "aaaaaaaaa",
+            "aaaaaaaaaa",
+        ]
+        self.assertEqual([], wordBreak(s, word_dict))
