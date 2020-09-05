502d7f515f0a9934128cb6aee351c63a6849cf87
Ben Lerner <benlerner95@gmail.com>
2020-09-04 22:45:36 -0400

    commented

--- moderate/count_bits.py
+++ moderate/count_bits.py
@@ -6,6 +6,10 @@ def is_power_of_two(num):
 
 
 def countBits(num: int) -> List[int]:
+    """
+    For each number, 0 through num, count the number of "1"s in the binary representation of each number.
+    Return a list containing the 1 bits of each number of the corresponding index.
+    """
     bits = [0 for _ in range(num + 1)]
     bits[0] = 0
     curr_exponent_of_two = -1
